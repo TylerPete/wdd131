74a066de8ab4f46090d745111bd47c894951e999
TylerPete <tpeterson4@byupathway.edu>
2025-05-10 22:09:10 -0500

    Potentially finished home page, including required javascript

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,1 +1,12 @@
 //meow
+
+const year = document.querySelector("#currentyear");
+const lastModifiedDate = document.querySelector("#lastModified");
+
+const today = new Date();
+
+year.textContent = `${today.getFullYear()}`;
+lastModifiedDate.textContent = `Last Modification: ${new Intl.DateTimeFormat("en-US", 
+{
+    dateStyle: "short", timeStyle: "medium"
+}).format(today)}`
