57ed5bbeb6128a500e18d4870b30e557b43d44c7
TylerPete <tpeterson4@byupathway.edu>
2025-05-28 16:57:21 -0500

    pushing majority of JavaScript completion for place page assignment

--- scripts/place.js
+++ scripts/place.js
@@ -1,3 +1,9 @@
+function calculateWindChill(temp, windSpeed) {
+    let chill = 35.74 + (0.6215 * temp) - (35.75 * (windSpeed ** 0.16)) + (0.4275 * temp * (windSpeed ** 0.16));
+
+    return chill;
+}
+
 const year = document.querySelector("#currentyear");
 const lastModified = document.querySelector("#lastModified");
 
@@ -9,3 +15,21 @@ lastModified.textContent = `Last Modification: ${new Intl.DateTimeFormat("en-US"
     {
         dateStyle: "short", timeStyle: "medium"
     }).format(dateLastModified)}`
+
+const tempElement = document.querySelector("#temperature-number");
+const tempNum = parseFloat(tempElement.textContent);
+
+const windElement = document.querySelector("#wind-number");
+const windNum = parseFloat(windElement.textContent);
+
+let windChillTemp;
+
+if (tempNum >= 50 || windNum < 3) {
+    windChillTemp = "N/A";
+} else {
+    let windChillNum = calculateWindChill(tempNum, windNum).toFixed(1);
+    windChillTemp = "" + windChillNum + " °F";
+}
+
+const windChillElement = document.querySelector("#wind-chill");
+windChillElement.textContent = windChillTemp;
