ec14aa1b95cee7434040d48ee359c7e93eddb653
TylerPete <tpeterson4@byupathway.edu>
2025-06-01 22:43:37 -0500

    finished temple filter menu functionality for wk4 project

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -27,7 +27,7 @@ const temples = [
     templeName: "Aba Nigeria",
     location: "Aba, Nigeria",
     dedicated: "2005, August, 7",
-    area: 11500,
+    size: 11500,
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
   },
@@ -35,7 +35,7 @@ const temples = [
     templeName: "Manti Utah",
     location: "Manti, Utah, United States",
     dedicated: "1888, May, 21",
-    area: 74792,
+    size: 74792,
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
   },
@@ -43,7 +43,7 @@ const temples = [
     templeName: "Payson Utah",
     location: "Payson, Utah, United States",
     dedicated: "2015, June, 7",
-    area: 96630,
+    size: 96630,
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
   },
@@ -51,7 +51,7 @@ const temples = [
     templeName: "Yigo Guam",
     location: "Yigo, Guam",
     dedicated: "2020, May, 2",
-    area: 6861,
+    size: 6861,
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
   },
@@ -59,7 +59,7 @@ const temples = [
     templeName: "Washington D.C.",
     location: "Kensington, Maryland, United States",
     dedicated: "1974, November, 19",
-    area: 156558,
+    size: 156558,
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
   },
@@ -67,7 +67,7 @@ const temples = [
     templeName: "Lima Perú",
     location: "Lima, Perú",
     dedicated: "1986, January, 10",
-    area: 9600,
+    size: 9600,
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
   },
@@ -75,28 +75,28 @@ const temples = [
     templeName: "Mexico City Mexico",
     location: "Mexico City, Mexico",
     dedicated: "1983, December, 2",
-    area: 116642,
+    size: 116642,
     imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
   },
   {
     templeName: "Winter Quarters Nebraska",
     location: "Omaha, Nebraska, United States",
     dedicated: "2001, April, 22",
-    area: 16000,
+    size: 16000,
     imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/winter-quarters-nebraska-temple/winter-quarters-nebraska-temple-317.jpg"
   },
   {
     templeName: "Nauvoo Illinois",
     location: "Nauvoo, Illinois, United States",
     dedicated: "2002, May, 27",
-    area: 54000,
+    size: 54000,
     imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/nauvoo-illinois-temple/nauvoo-illinois-temple-50576.jpg"
   },
   {
     templeName: "Chicago Illinois",
     location: "Chicago, Illinois, United States",
     dedicated: "1985, August, 9",
-    area: 37062,
+    size: 37062,
     imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/chicago-illinois-temple/chicago-illinois-temple-58403.jpg"
   }
 ];
@@ -105,10 +105,10 @@ function createTempleCardHTML(temple)
 {
     let html = `<div class="temple-card">\n<h3>${temple.templeName}</h3>\n<ul>\n`;
 
-    let values = ["location", "dedicated", "area"];
-    let listElementArray = values.map(value => value = `<li><span>LOCATION: </span>${temple[value]}</li>`);
+    let values = ["location", "dedicated", "size"];
+    let listElementArray = values.map(value => value = `<li><span>${value.toUpperCase()}: </span>${temple[value]}</li>`);
 
-    html += `${listElementArray.join("\n")}\n</ul>\n<img src="${temple.imageUrl}" alt="${temple.templeName}">\n</div>`;
+    html += `${listElementArray.join("\n")}\n</ul>\n<img src="${temple.imageUrl}" alt="${temple.templeName}" loading="lazy">\n</div>`;
 
     return html;
 }
@@ -118,3 +118,63 @@ let finalHTML = templeCardsHTMLArray.join("\n");
 
 const albumGridElement = document.querySelector(".album-grid");
 albumGridElement.innerHTML = finalHTML;
+
+//"Old" Temples Filter and Listener
+const oldLink = document.querySelector("#old-link");
+oldLink.addEventListener("click", () => {
+    let oldTemples = temples.filter(temple => isOldEnough(temple));
+    filterCards(oldTemples);
+});
+
+//"New" Temples Filter and Listener
+const newLink = document.querySelector("#new-link");
+newLink.addEventListener("click", () => {
+    let newTemples = temples.filter(temple => isNewEnough(temple));
+    filterCards(newTemples);
+});
+
+//"Large" Temples Filter and Listener
+const largeLink = document.querySelector("#large-link");
+largeLink.addEventListener("click", () => {
+    let largeTemples = temples.filter(temple => isLargeEnough(temple));
+    filterCards(largeTemples);
+});
+
+//"Small" Temples Filter and Listener
+const smallLink = document.querySelector("#small-link");
+smallLink.addEventListener("click", () => {
+    let smallTemples = temples.filter(temple => isSmallEnough(temple));
+    filterCards(smallTemples);
+});
+
+//"Home" Filter (show all temple cards)
+const homeLink = document.querySelector("#home-link");
+homeLink.addEventListener("click", () => {
+    filterCards(temples);
+})
+
+
+//Boolean return functions for filter criteria
+function isOldEnough(temple) {
+    return temple.dedicated.split(",")[0] < 1900;
+}
+
+function isNewEnough(temple) {
+    return temple.dedicated.split(",")[0] > 2000;
+}
+
+function isLargeEnough(temple) {
+    return temple.size > 90000;
+}
+
+function isSmallEnough(temple) {
+    return temple.size < 10000;
+}
+
+//Helper function called by event listeners to reset innerHTML of the grid
+function filterCards(newTemplesArray) {
+    let newTempleCardsHTMLArray = newTemplesArray.map(createTempleCardHTML);
+    let newFinalHTML = newTempleCardsHTMLArray.join("\n");
+
+    albumGridElement.innerHTML = newFinalHTML;
+}
