4cca03a2bfa47655fe7bb602037ba6d7c1ca8032
tylerpete <tpeterson4@byupathway.edu>
2025-06-06 18:12:17 -0500

    files in place for BOM favorite chapters 2.0 (learning activity for week 5 with localStorage)

---
+++ week05/scripts/bom.js
@@ -0,0 +1,27 @@
+const inp = document.querySelector("#favchap");
+const addButt = document.querySelector("button");
+const list = document.querySelector("#list");
+
+addButt.addEventListener("click", function () {
+    if (inp.value.trim() == "") {
+        inp.focus();
+    } else {
+        const chap = document.createElement("li");
+        const delButt = document.createElement("button");
+
+        chap.textContent = inp.value;
+        delButt.textContent = "❌";
+
+        chap.appendChild(delButt);
+        list.appendChild(chap);
+        inp.focus();
+
+        delButt.addEventListener("click", function () {
+            list.removeChild(chap);
+            input.focus();
+        });
+
+        inp.value = "";
+    }
+});
+
