871a61022775a73fcf19e66ba822ed5089407ae1
tylerpete <tpeterson4@byupathway.edu>
2025-06-09 15:39:10 -0500

    typed out form element, and product name, overall rating, & date of installation input elements in HTML for Product Review Form Wk5 assignment

--- scripts/form.js
+++ scripts/form.js
@@ -1,3 +1,32 @@
+//Product array (provided)
+const products = [
+    {
+        id: "fc-1888",
+        name: "flux capacitor",
+        averagerating: 4.5
+    },
+    {
+        id: "fc-2050",
+        name: "power laces",
+        averagerating: 4.7
+    },
+    {
+        id: "fs-1987",
+        name: "time circuits",
+        averagerating: 3.5
+    },
+    {
+        id: "ac-2000",
+        name: "low voltage reactor",
+        averagerating: 3.9
+    },
+    {
+        id: "jj-1969",
+        name: "warp equalizer",
+        averagerating: 5.0
+    }
+];
+
 const year = document.querySelector("#currentyear");
 const lastModified = document.querySelector("#lastModified");
 
@@ -9,3 +38,7 @@ lastModified.textContent = `Last Modification: ${new Intl.DateTimeFormat("en-US"
     {
         dateStyle: "short", timeStyle: "medium"
     }).format(dateLastModified)}`;
+
+
+//Code to generate <option> children elements for <select> element...
+//  ...see "Product Name" portion of directions
