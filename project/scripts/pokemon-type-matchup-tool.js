5e3c635d1d1ec9786fecb93cf86aa111fde0b0d3
TylerPete <tpeterson4@byupathway.edu>
2025-06-12 23:29:17 -0500

    in the middle of troubleshoot stupid grid issues with the type icons. Just causing more problems, not really fixing

--- project/scripts/pokemon-type-matchup-tool.js
+++ project/scripts/pokemon-type-matchup-tool.js
@@ -1,1 +1,9 @@
-//meow
+const menuButton = document.querySelector("#menu");
+menuButton.addEventListener("click", showMenu)
+const navMenu = document.querySelector(".navigation");
+
+function showMenu()
+{
+    navMenu.classList.toggle("open");
+    menuButton.classList.toggle("open");
+}
