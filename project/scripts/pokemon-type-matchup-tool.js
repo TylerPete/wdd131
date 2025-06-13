9c65860f53ff6b4ec3907d53ddf72f0f9a817705
TylerPete <tpeterson4@byupathway.edu>
2025-06-13 08:55:18 -0500

    coded type icon selection with a max of 2 and formatted the pokemon example image (with a placeholder togepi). next, need to use javascript to make pokemon display according to the selected type(s)... see PokeAPI??

--- project/scripts/pokemon-type-matchup-tool.js
+++ project/scripts/pokemon-type-matchup-tool.js
@@ -2,8 +2,29 @@ const menuButton = document.querySelector("#menu");
 menuButton.addEventListener("click", showMenu)
 const navMenu = document.querySelector(".navigation");
 
+const typeIcons = document.querySelectorAll(".type-icon");
+const iconArray = Array.from(typeIcons);
+iconArray.forEach(icon => {
+    icon.addEventListener("click", function() {
+        numIconsSelected = document.getElementsByClassName("selected").length;
+
+        if (icon.classList.contains("selected"))
+        {
+            icon.classList.toggle("selected");
+        }
+        else
+        {
+            if (numIconsSelected < 2)
+            {
+                icon.classList.toggle("selected");
+            }
+        }
+    });
+});
+
 function showMenu()
 {
     navMenu.classList.toggle("open");
     menuButton.classList.toggle("open");
 }
+
