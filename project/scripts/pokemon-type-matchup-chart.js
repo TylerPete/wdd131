390c5660ed884347437893c0449aa5c1de0780eb
TylerPete <tpeterson4@byupathway.edu>
2025-06-18 14:54:55 -0500

    implemented javascript sortTable function, with a "click" event listener to be added to every attack type header. when clicked, the attack type header will send its type to the sortTable() function to order the defending types by their level of effectedness from that attack type

--- project/scripts/pokemon-type-matchup-chart.js
+++ project/scripts/pokemon-type-matchup-chart.js
@@ -27,4 +27,95 @@ function showMenu() {
     menuButton.classList.toggle("x");
 }
 
-// const tableResetButton = document.querySelector("#table-reset-button");
+const types = [
+    { type: "Bug", super: ["Grass", "Psychic", "Dark"], notvery: ["Fire", "Fighting", "Poison", "Flying", "Ghost", "Steel", "Fairy"], noeffect: [] },
+    { type: "Dark", super: ["Psychic", "Ghost"], notvery: ["Fighting", "Dark", "Fairy"], noeffect: [] },
+    { type: "Dragon", super: ["Dragon"], notvery: ["Steel"], noeffect: ["Fairy"] },
+    { type: "Electric", super: ["Water", "Flying"], notvery: ["Electric", "Grass", "Dragon"], noeffect: ["Ground"] },
+    { type: "Fairy", super: ["Fighting", "Dragon", "Dark"], notvery: ["Fire", "Poison", "Steel"], noeffect: [] },
+    { type: "Fighting", super: ["Normal", "Ice", "Rock", "Dark", "Steel"], notvery: ["Poison", "Flying", "Psychic", "Bug", "Fairy"], noeffect: ["Ghost"] },
+    { type: "Fire", super: ["Grass", "Ice", "Bug", "Steel"], notvery: ["Fire", "Water", "Rock", "Dragon"], noeffect: [] },
+    { type: "Flying", super: ["Grass", "Fighting", "Bug"], notvery: ["Electric", "Rock", "Steel"], noeffect: [] },
+    { type: "Ghost", super: ["Psychic", "Ghost"], notvery: ["Dark"], noeffect: ["Normal"] },
+    { type: "Grass", super: ["Water", "Ground", "Rock"], notvery: ["Fire", "Grass", "Poison", "Flying", "Bug", "Dragon", "Steel"], noeffect: [] },
+    { type: "Ground", super: ["Fire", "Electric", "Poison", "Rock", "Steel"], notvery: ["Grass", "Bug"], noeffect: ["Flying"] },
+    { type: "Ice", super: ["Grass", "Ground", "Flying", "Dragon"], notvery: ["Fire", "Water", "Ice", "Steel"], noeffect: [] },
+    { type: "Normal", super: [], notvery: ["Rock", "Steel"], noeffect: ["Ghost"] },
+    { type: "Poison", super: ["Grass", "Fairy"], notvery: ["Poison", "Ground", "Rock", "Ghost"], noeffect: ["Steel"] },
+    { type: "Psychic", super: ["Fighting", "Poison"], notvery: ["Psychic", "Steel"], noeffect: ["Dark"] },
+    { type: "Rock", super: ["Fire", "Ice", "Flying", "Bug"], notvery: ["Fighting", "Ground", "Steel"], noeffect: [] },
+    { type: "Steel", super: ["Ice", "Rock", "Fairy"], notvery: ["Fire", "Water", "Electric", "Steel"], noeffect: [] },
+    { type: "Water", super: ["Fire", "Ground", "Rock"], notvery: ["Water", "Grass", "Dragon"], noeffect: [] }
+]
+
+const tableResetButton = document.querySelector("#table-reset-button");
+// tableResetButton.addEventListener("click", resetTable()); //Implement separate function for this!!!
+
+const attackTypeCellsNodeList = document.querySelectorAll(".attack-type-cell");
+for (const cell of attackTypeCellsNodeList)
+{
+    const theTypeObj = types.find(type => type.type == cell.textContent);
+
+    cell.addEventListener("click", () => sortTable(theTypeObj));
+}
+
+function sortTable(attackTypeObj)
+{
+    const sortedTypesArray = types.toSorted(defenseTypeCompareFunction);
+
+    const defenseTypeCellsNodeList = document.querySelectorAll(".defense-type-cell");
+    
+    for (let i = 0; i < 18; i++)
+    {
+        const node = defenseTypeCellsNodeList[i];
+        node.textContent = sortedTypesArray[i].type;
+    }
+
+    //meow
+
+    function defenseTypeCompareFunction(defTypeObjA, defTypeObjB)
+    {
+        let damageModifierA = getDamageModifier(defTypeObjA);
+        let damageModifierB = getDamageModifier(defTypeObjB);
+
+        let returnValue;
+        if (damageModifierA > damageModifierB)
+        {
+            returnValue = -1;
+        }
+        else if (damageModifierA < damageModifierB)
+        {
+            returnValue = 1;
+        }
+        else
+        {
+            returnValue = 0;
+        }
+
+        return returnValue;
+    }
+
+    function getDamageModifier(defTypeObj)
+    {
+        let damageModifier;
+
+        if (attackTypeObj.super.includes(defTypeObj.type))
+        {
+            damageModifier = 2;
+        }
+        else if (attackTypeObj.notvery.includes(defTypeObj.type))
+        {
+            damageModifier = 0.5;
+        }
+        else if (attackTypeObj.noeffect.includes(defTypeObj.type))
+        {
+            damageModifier = 0;
+        }
+        else
+        {
+            damageModifier = 1;
+        }
+
+        return damageModifier;
+    }
+}
