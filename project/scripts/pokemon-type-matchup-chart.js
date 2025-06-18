4c810a5b96372492ea41c237c20b52151bc1de6e
TylerPete <tpeterson4@byupathway.edu>
2025-06-18 08:00:48 -0500

    set up blank (besides header and footer) page for type chart, along with mostly blank javscript file including just menu, resizing, and currentyear handling for the footer

---
+++ project/scripts/pokemon-type-matchup-chart.js
@@ -0,0 +1,29 @@
+const menuButton = document.querySelector("#menu");
+menuButton.addEventListener("click", showMenu)
+const navMenu = document.querySelector("#navigation");
+
+const currentYear = document.querySelector(".current-year");
+const today = new Date();
+currentYear.textContent = `${today.getFullYear()}`;
+
+
+window.addEventListener("resize", () => {
+    if (window.innerWidth >= 640)
+    {
+        if (navMenu.classList.contains("open"))
+        {
+            navMenu.classList.remove("open");
+
+        }
+        if (menuButton.classList.contains("x"))
+        {
+            menuButton.classList.remove("x");
+        }
+    }
+});
+
+
+function showMenu() {
+    navMenu.classList.toggle("open");
+    menuButton.classList.toggle("x");
+}
